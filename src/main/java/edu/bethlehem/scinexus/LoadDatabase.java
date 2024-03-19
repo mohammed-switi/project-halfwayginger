418281690d39cc6eec960e96f0637808272aa88a
Mohammed Sowaity <switi.2003@gmail.com>
2024-03-19 03:16:10 +0200

    I have handled the Sql integrity exceptions realted to Email, and username in the register Controller, But still there is problem with the database loading, I have commented the lines for the email and user name configuration to be unique, so now they are not unique, but IT MUST BE FIXED!!

--- src/main/java/edu/bethlehem/scinexus/LoadDatabase.java
+++ src/main/java/edu/bethlehem/scinexus/LoadDatabase.java
@@ -28,6 +28,7 @@ import edu.bethlehem.scinexus.ResearchPaper.ResearchPaper;
 import edu.bethlehem.scinexus.ResearchPaper.ResearchPaperRepository;
 import edu.bethlehem.scinexus.Academic.Academic;
 import edu.bethlehem.scinexus.Media.MediaType;
+import org.springframework.dao.DataIntegrityViolationException;
 
 @Configuration
 class LoadDatabase {
@@ -47,99 +48,133 @@ class LoadDatabase {
       // generate academics
       List<Name> names = generator.generateNames(bound);
       ArrayList<Academic> academics = new ArrayList<Academic>();
+      int k =0;
       for (Name name : names) {
         academics.add(new Academic(name.toString(), name.getFirstName(),
-            "pass1234" + name.getFirstName(), name.getLastName() + name.getFirstName() + "@bethlehem.edu"));
-
+            "pass1234" + name.getFirstName(), name.getLastName() + name.getFirstName() +"" +new Random(k).nextInt() + "@bethlehem.edu"));
+      k++;
       }
-      academicRepository.saveAll(academics);
+      try {
 
-      // generate articles
-      ArrayList<Article> articles = new ArrayList<Article>();
-      for (int i = 0; i < bound; i++) {
-        Academic academic = academics.get(random.nextInt(academics.size()));
-        articles.add(new Article("title" + academic.getName(), "description" + academic.getEmail(), "subject", academic,
-            "content"));
+        academicRepository.saveAll(academics);
+      }catch (DataIntegrityViolationException exception){
+        System.out.println("DuplicateEntry");
       }
-      articleRepository.saveAll(articles);
-
-      // generate Organizations
-      ArrayList<Organization> organizations = new ArrayList<Organization>();
-      for (int i = 0; i < bound; i++) {
-        Academic academic = academics.get(random.nextInt(academics.size()));
-        organizations.add(new Organization("name" + academic.getName(), "username" + academic.getName(),
-            "password" + academic.getName(), "email" + academic.getName(), "type"));
-        if (random.nextInt(3) == 0) {
-          organizations.get(i).setVerified(true);
+        // generate articles
+        ArrayList<Article> articles = new ArrayList<Article>();
+        for (int i = 0; i < bound; i++) {
+          Academic academic = academics.get(random.nextInt(academics.size()));
+          articles.add(new Article("title" + academic.getName(), "description" + academic.getEmail(), "subject", academic,
+                  "content"));
         }
-        if (random.nextInt(3) == 0) {
-          Integer rand = random.nextInt(academics.size());
-          Academic academicT = academics.get(rand);
-          // academicT.setOrganization(organizations.get(i));
-          academicT.setBadge("badge" + academicT.getName());
-          academicT.setPosition(Position.values()[random.nextInt(Position.values().length)]);
-          academicRepository.save(academicT);
-        }
-      }
-      organizationRepository.saveAll(organizations);
+        articleRepository.saveAll(articles);
 
-      // generate posts
-      ArrayList<Post> posts = new ArrayList<Post>();
-      for (int i = 0; i < bound; i++) {
-        Academic academic = academics.get(random.nextInt(academics.size()));
-        posts.add(new Post("Content" + academic.getName(), academic));
-      }
-      postRepository.saveAll(posts);
-
-      // generate ResearchPapers
-      ArrayList<ResearchPaper> researchPapers = new ArrayList<ResearchPaper>();
-      for (int i = 0; i < bound; i++) {
-        Academic academic = academics.get(random.nextInt(academics.size()));
-        researchPapers
-            .add(new ResearchPaper("Title" + academic.getName(), "Description" + academic.getEmail(), "Subject",
-                academic));
-      }
-      researchPaperRepository.saveAll(researchPapers);
-
-      // generate media
-      ArrayList<Media> media = new ArrayList<Media>();
-      for (int i = 0; i < bound * 5; i++) {
-        Media media1 = new Media(MediaType.IMAGE, "url" + i);
-
-        switch (random.nextInt(4)) {
-          case 0:
-
-            media1.setOwnerPost(posts.get(random.nextInt(posts.size())));
-
-          case 1:
-
-            media1.setOwnerArticle(articles.get(random.nextInt(articles.size())));
-
-          case 2:
-            media1.setOwnerResearchPaper(researchPapers.get(random.nextInt(researchPapers.size())));
-          case 3:
+        // generate Organizations
+        ArrayList<Organization> organizations = new ArrayList<Organization>();
+        for (int i = 0; i < bound; i++) {
+          Academic academic = academics.get(random.nextInt(academics.size()));
+          organizations.add(new Organization("name" + academic.getName(), "username" + academic.getName() +""+new Random(i).nextInt(),
+                  "password" + academic.getName(), "email" + academic.getName(), "type"));
+          if (random.nextInt(3) == 0) {
+            organizations.get(i).setVerified(true);
+          }
+          if (random.nextInt(3) == 0) {
             Integer rand = random.nextInt(academics.size());
             Academic academicT = academics.get(rand);
-            if (media1.getOwnerPost() == null && media1.getOwnerArticle() == null
-                && media1.getOwnerResearchPaper() == null) {
-              academicT.setProfilePicture(media1);
+            // academicT.setOrganization(organizations.get(i));
+            academicT.setBadge("badge" + academicT.getName());
+            academicT.setPosition(Position.values()[random.nextInt(Position.values().length)]);
+
+            try {
               academicRepository.save(academicT);
+            }catch (DataIntegrityViolationException exception){
+              academicRepository.delete(academicT);
+              System.out.println("DuplicateEntry");
             }
-          case 4:
-            Integer rand1 = random.nextInt(academics.size());
-            Academic academicT1 = academics.get(rand1);
-            if (media1.getOwnerPost() == null && media1.getOwnerArticle() == null
-                && media1.getOwnerResearchPaper() == null) {
-
-              academicT1.setProfileCover(media1);
-              academicRepository.save(academicT1);
-            }
-
+          }
         }
-        media.add(media1);
+      try {
+        organizationRepository.saveAll(organizations);
+      }catch (DataIntegrityViolationException exception){
+        System.out.println("DuplicateEntry");
       }
-      mediaRepository.saveAll(media);
-      System.out.println("Preloading ");
+
+        // generate posts
+        ArrayList<Post> posts = new ArrayList<Post>();
+        for (int i = 0; i < bound; i++) {
+          Academic academic = academics.get(random.nextInt(academics.size()));
+          posts.add(new Post("Content" + academic.getName(), academic));
+        }
+      try {
+        postRepository.saveAll(posts);
+      }catch (DataIntegrityViolationException exception){
+        System.out.println("DuplicateEntry post");
+      }
+
+        // generate ResearchPapers
+        ArrayList<ResearchPaper> researchPapers = new ArrayList<ResearchPaper>();
+        for (int i = 0; i < bound; i++) {
+          Academic academic = academics.get(random.nextInt(academics.size()));
+          researchPapers
+                  .add(new ResearchPaper("Title" + academic.getName(), "Description" + academic.getEmail(), "Subject",
+                          academic));
+        }
+        researchPaperRepository.saveAll(researchPapers);
+
+        // generate media
+        ArrayList<Media> media = new ArrayList<Media>();
+        for (int i = 0; i < bound * 5; i++) {
+          Media media1 = new Media(MediaType.IMAGE, "url" + i);
+
+          switch (random.nextInt(4)) {
+            case 0:
+
+              media1.setOwnerPost(posts.get(random.nextInt(posts.size())));
+
+            case 1:
+
+              media1.setOwnerArticle(articles.get(random.nextInt(articles.size())));
+
+            case 2:
+              media1.setOwnerResearchPaper(researchPapers.get(random.nextInt(researchPapers.size())));
+            case 3:
+              Integer rand = random.nextInt(academics.size());
+              Academic academicT = academics.get(rand);
+              if (media1.getOwnerPost() == null && media1.getOwnerArticle() == null
+                      && media1.getOwnerResearchPaper() == null) {
+                academicT.setProfilePicture(media1);
+                try {
+                  academicRepository.save(academicT);
+                }catch (DataIntegrityViolationException exception){
+                  academicRepository.delete(academicT);
+                  System.out.println("DuplicateEntry");
+                }
+              }
+            case 4:
+              Integer rand1 = random.nextInt(academics.size());
+              Academic academicT1 = academics.get(rand1);
+              if (media1.getOwnerPost() == null && media1.getOwnerArticle() == null
+                      && media1.getOwnerResearchPaper() == null) {
+
+                academicT1.setProfileCover(media1);
+                try {
+                  academicRepository.save(academicT1);
+                }catch (DataIntegrityViolationException exception){
+                  academicRepository.delete(academicT1);
+                }
+              }
+
+          }
+          media.add(media1);
+        }
+        try {
+
+        mediaRepository.saveAll(media);
+        }catch (DataIntegrityViolationException e){
+          System.out.println("Duplication");
+        }
+        System.out.println("Preloading ");
+
 
     };
   }
